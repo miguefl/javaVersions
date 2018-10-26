ebb0ab32ccc644a939d6264b8a064f0fefaebff9
Miguel Fernández <27681557+miguefl@users.noreply.github.com>
2018-10-26 19:44:32 +0200

    update with java12

--- javaVersions.plugin.zsh
+++ javaVersions.plugin.zsh
@@ -5,11 +5,13 @@ export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 export JAVA_9_HOME=$(/usr/libexec/java_home -v9)
 export JAVA_10_HOME=$(/usr/libexec/java_home -v10)
 export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+export JAVA_12_HOME=$(/usr/libexec/java_home -v12)
 
 alias java8='export JAVA_HOME=$JAVA_8_HOME'
 alias java9='export JAVA_HOME=$JAVA_9_HOME'
 alias java10='export JAVA_HOME=$JAVA_10_HOME'
 alias java11='export JAVA_HOME=$JAVA_11_HOME'
+alias java12='export JAVA_HOME=$JAVA_12_HOME'
 
 #default java8
 export JAVA_HOME=$JAVA_8_HOME
